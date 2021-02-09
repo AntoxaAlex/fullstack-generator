8b5e03ddbe2ca248196df6ea8f058caf3ddeaf93
Anton Pertsevoy <antoxaalex@gmail.com>
2021-02-09 16:16:06 +0200

    Create server side v1.0

---
+++ routes/user.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const router = express.Router({mergeParams: true});
+const {body, validationResult} = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+
+if (process.env.NODE_ENV !== 'production') {
+    require("dotenv").config()
+}
+
+//Login user
+
+//Logout user
+
+//Register user
+router.post("/",[
+    //name is required
+    body("firstname","First name is required").not().isEmpty(),
+    //secondname is required
+    body("secondname","Second name is required").not().isEmpty(),
+    //username must be email
+    body('email',"Please include a valid email").isEmail(),
+    //min length of password must be 5
+    body('password',"Please enter a password with 5 or more characters").isLength({min: 5})
+], async (req,res)=>{
+    //Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()})
+    }
+//    Retrieve data from body
+    const {firstname, secondname, email, password} = req.body
+    try {
+        //    Check if the user is already exist
+        let user = await User.findOne({email})
+        if(user){
+            return res.status(400).json({errors: [{msg: "User already exist"}]})
+        }
+        user = new User({
+            firstname,
+            secondname,
+            email,
+            password
+        })
+
+        //    Encrypt password
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password,salt);
+        await user.save();
+
+        //    Return jsonwebtoken
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+        const secret = process.env.SECRETKEY
+        jwt.sign(payload,secret,{expiresIn: 360000},(err,token)=>{
+            if(err) throw err;
+            res.json({token})
+        })
+        console.log("New user has been created")
+
+    }catch (e) {
+        console.log(e.message)
+    }
+})
+
+
+module.exports = router
