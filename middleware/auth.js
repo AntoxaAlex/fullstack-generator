856ee2e5ae256710848656241062aef823dbb633
Anton Pertsevoy <antoxaalex@gmail.com>
2021-03-30 22:47:01 +0300

    Fix server bugs

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,20 +3,19 @@ if (process.env.NODE_ENV !== 'production') {
     require("dotenv").config();
 }
 
-module.exports = async (req, res, next)  =>{
+module.exports = function (req, res, next) {
     //Get token from the header
-    const token = await req.header("x-auth-token");
+    const token = req.header("x-auth-token");
     //Get secret from .env
-    const secret =await process.env.SECRETKEY;
+    const secret = process.env.SECRETKEY;
 
     //Check if no token
-    if(!req.header("x-auth-token")){
-        return res.status(401).json({msg: "You have no token, authorisation denied"});
+    if(!token){
+        return res.status(401).json({msg: "No token, authorisation denied"});
     }
 
     //Verify token
     try {
-        console.log(req.header("x-auth-token"))
         const decoded = jwt.verify(token,secret);
         req.user = decoded.user;
         next();
